fe84424fc52b051bda6169972caae75ba88a7f10
bella <aissatoubella0127@gmail.com>
2018-06-25 14:39:54 +0200

    Added view and template for detailed blog post as well as Css for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    Post.objects.get(pk=pk)
+    # http://127.0.0.1:8000/post/2/
+    url(r'^post/(?P<id>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,8 @@ from .models import Binta
 def post_list(request):
     posts = Binta.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Binta, pk=id)
     return render(request, 'blog/post_detail.html', {'post': post})
